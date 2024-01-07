bea354f5c52a4aa1360fc47156481ec4d209a9c1
p-muluk <muluk.prakash404@gmail.com>
2024-01-07 13:32:58 +0530

    initial commit

---
+++ src/test/java/com/pramul/dbmigration/liquibaseexample/LiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramul.dbmigration.liquibaseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
